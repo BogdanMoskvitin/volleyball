00d23778d1b95b31045492438043cdd5d2135e73
Bogdan Moskvitin <moskvitin.bg@gmail.com>
2022-01-19 13:15:33 +0300

    add auto check auth

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './classes/auth.guard';
 
 const routes: Routes = [
+  { path: '', redirectTo: 'main', pathMatch: 'full' },
   { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) }, 
   { path: 'main', canActivate: [AuthGuard], loadChildren: () => import('./main/main.module').then(m => m.MainModule) }, 
-  { path: '', redirectTo: 'auth', pathMatch: 'full' }
+  { path: '**', redirectTo: 'auth', pathMatch: 'full' }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [AuthGuard]
 })
 export class AppRoutingModule { } 
