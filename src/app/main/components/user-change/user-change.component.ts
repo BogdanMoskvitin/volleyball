b73f392540eb3bd04dcf74c6754ee48a26de0e8c
Bogdan Moskvitin <moskvitin.bg@gmail.com>
2022-01-24 16:47:50 +0300

    create change user

--- src/app/main/components/user-change/user-change.component.ts
+++ src/app/main/components/user-change/user-change.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { MyData } from 'src/app/my-data.service';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Component({
     selector: 'user-change-service-page',
@@ -12,24 +14,33 @@ export class UserChangeComponent implements OnInit {
 
     changeForm : FormGroup;
     mydata;
+    url:string = environment.apiUrl;
+    id: number;
 
-    constructor(private myData: MyData) { }
+    constructor(
+        private myData: MyData,
+        private http: HttpClient
+    ) { }
     
     ngOnInit() {
         this.myData.currentData.subscribe(
             (res) => {
                 this.mydata = res;
+                this.id = this.mydata.id;
                 this.changeForm = new FormGroup({
-                    first_name: new FormControl(this.mydata.first_name, [Validators.required]),
-                    last_name: new FormControl(this.mydata.last_name, [Validators.required]),
-                    username: new FormControl(this.mydata.email, [Validators.required]),
-                    phone_number: new FormControl(this.mydata.phone_number, [Validators.required]),
+                    first_name: new FormControl(this.mydata.first_name),
+                    last_name: new FormControl(this.mydata.last_name),
+                    email: new FormControl(this.mydata.email),
+                    phone_number: new FormControl(this.mydata.phone_number),
                 });
             }
         );
     }
 
     sendService(){
-        console.log(this.changeForm.value);
+        return this.http.patch(this.url + `auth/users/${this.id}`, this.changeForm.value)
+            .subscribe((res) => {
+                window.location.reload();
+        });
     }
 }
