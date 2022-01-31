01eee6968f62c56e7311ed152c706da2f383f25e
Bogdan Moskvitin <moskvitin.bg@gmail.com>
2022-01-31 10:47:11 +0300

    add kind/type/status events

--- src/app/main/components/add-event/add-event.component.ts
+++ src/app/main/components/add-event/add-event.component.ts
@@ -17,12 +17,16 @@ export class AddEventComponent implements OnInit {
     events;
     locations;
     url:string = environment.apiUrl;
+    kinds;
+    types;
 
     constructor(
         private http: HttpClient, 
         private datePipe: DatePipe, 
         private router: Router) {
         this.addEventForm = new FormGroup({
+            kind: new FormControl('', Validators.required),
+            type: new FormControl('', Validators.required),
             date: new FormControl('', Validators.required),
             time: new FormControl('', Validators.required),
             location: new FormControl('', Validators.required),
@@ -33,6 +37,8 @@ export class AddEventComponent implements OnInit {
     ngOnInit() {
         this.getEvents();
         this.getLocations();
+        this.getKinds();
+        this.getTypes();
     }
 
     getEvents() {
@@ -46,9 +52,11 @@ export class AddEventComponent implements OnInit {
 
     sendService(){
         this.newEvent = {
+            kind: this.addEventForm.value.kind,
+            type: this.addEventForm.value.type,
             time_start: (this.datePipe.transform(this.addEventForm.value.date, 'yyyy-MM-dd') + 'T' + this.addEventForm.value.time),
             location: this.addEventForm.value.location,
-            price: this.addEventForm.value.price
+            price: this.addEventForm.value.price,
         }
         return this.http.post(this.url + 'events/', this.newEvent)
             .subscribe((res) => {
@@ -60,6 +68,19 @@ export class AddEventComponent implements OnInit {
         return this.http.get(this.url + 'locations/')
             .subscribe((res) => {
                 this.locations = res;
+        });
+    }
+
+    getKinds() {
+        return this.http.get(this.url + 'kinds/')
+            .subscribe((res) => {
+                this.kinds = res;
+        });
+    }
+    getTypes() {
+        return this.http.get(this.url + 'types/')
+            .subscribe((res) => {
+                this.types = res;
         });
     }
 }
