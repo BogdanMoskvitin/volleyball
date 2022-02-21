dd197c1d732bb08174a246f49c3feb45d81b588d
BogdanMoskvitin <moskvitin.bg@gmail.com>
2022-02-21 18:14:47 +0300

    create style event

--- src/app/main/components/event/event.component.ts
+++ src/app/main/components/event/event.component.ts
@@ -1,145 +1,3 @@
-// import { HttpClient } from '@angular/common/http';
-// import { Component, OnDestroy, OnInit } from '@angular/core';
-// import { ActivatedRoute } from '@angular/router';
-// import { MyData } from 'src/app/my-data.service';
-// import { Router } from '@angular/router';
-// import { FormControl, FormGroup, Validators } from '@angular/forms';
-// import { environment } from 'src/environments/environment';
-// import { ToastrService } from 'ngx-toastr';
-// import { Subscription } from 'rxjs';
-
-// @Component({
-//     selector: 'event-service-page',
-//     templateUrl: './event.component.html',
-//     styleUrls: ['./event.component.scss'],
-// })
-
-// export class EventComponent implements OnInit, OnDestroy {
-
-//     events;
-//     event;
-//     id: number;
-//     url: string = environment.apiUrl;
-//     idUser;
-//     players;
-//     addStatusForm: FormGroup;
-//     btnTitle: string = '';
-//     aSub1: Subscription;
-//     aSub2: Subscription;
-//     aSub4: Subscription;
-//     aSub5: Subscription;
-//     aSub6: Subscription;
-//     addCommentForm: FormGroup;
-//     qwerty;
-
-//     constructor(
-//         private activateRoute: ActivatedRoute,
-//         private http: HttpClient, 
-//         private myData: MyData,
-//         private router: Router,
-//         private toastr: ToastrService
-//         ) {
-//             this.id = this.activateRoute.snapshot.params['id'];
-//             this.addStatusForm = new FormGroup({
-//             player: new FormControl('', Validators.required)
-//         })
-//         this.addCommentForm = new FormGroup({
-//             comment: new FormControl('', Validators.required)
-//         })
-//     }
-
-//     sendComment(){
-//         this.http.post(this.url + `events/all/${this.id}/comments/`, {comment: this.addCommentForm.value.comment, player: 6, event: this.id}).subscribe(res => console.log(res))
-//     }
-
-//     ngOnInit() {
-//         this.aSub1 = this.myData.currentData.subscribe((res) => {
-//             this.idUser = res;
-//             console.log('id1:' + this.idUser.id)
-//             this.getPlayers();
-//             console.log('id2:' + this.idUser.id)
-//             this.getPlayer();
-//         });
-        
-//         this.aSub2 = this.http.get(this.url + `events/all/${this.id}/`)
-//             .subscribe((res) => {
-//                 this.event = res;
-//                 this.checkPlayer();
-//         });   
-//     }
-
-//     getPlayer(){
-//         console.log('id3:' + this.idUser.id)
-//         this.http.get(this.url + `events/all/${this.id}/surveys?player__user=${this.idUser.id}`)
-//         .subscribe((res) => {
-//             console.log(res)
-//         });
-//     }
-
-//     getPlayers() {
-//         console.log('id4:' + this.idUser.id)
-//         this.aSub6 = this.http.get(this.url + `players/all/?id=${this.idUser.id}`)
-//             .subscribe((res) => {
-//                 this.players = res;
-//         });
-//     }
-
-//     checkPlayer(){
-//         if((this.event.players).length != 0) {
-//             this.event.players.forEach(key => {
-//                 if(this.idUser.id == key.user.id){
-//                     this.btnTitle = 'Не приду';
-//                 } else {
-//                     this.btnTitle = 'Приду';
-//                 }
-//             });
-//         } else {
-//             this.btnTitle = 'Приду';
-//         }
-//     }
-
-//     sendStatus() {
-//         this.aSub4 = this.http.post(this.url + `events/all/${this.id}/participation/`, this.addStatusForm.value)
-//             .subscribe((res) => {
-//                 this.aSub5 = this.http.get(this.url + `events/all/${this.id}/`).subscribe(
-//                     (res) => {
-//                         this.toastr.success('Вы проголосовали!');
-//                         this.event = res;
-//                         this.checkPlayer();
-//                     },
-//                     error => {
-//                         this.toastr.error('Ошибка голосования');
-//                     });
-//         })
-//     }
-
-//     back(){
-//         this.router.navigateByUrl('main/header/home');
-//     }
-
-//     ngOnDestroy(){
-//         this.aSub1.unsubscribe();
-//         this.aSub2.unsubscribe();
-//         if(this.aSub4){
-//             this.aSub4.unsubscribe();
-//         }
-//         if(this.aSub5){
-//             this.aSub5.unsubscribe();
-//         }
-//         this.aSub6.unsubscribe();
-//     }
-
-//     sendTrue(){
-//         this.http.post(this.url + `events/all/${this.id}/surveys/`, {answer: true, player: this.addStatusForm.value.player, event: +this.id}).subscribe(res => console.log(res));
-//     }
-//     sendFalse(){
-//         this.http.post(this.url + `events/all/${this.id}/surveys/`, {answer: false, player: this.addStatusForm.value.player, event: +this.id}).subscribe(res => console.log(res));
-//     }
-
-
-// }
-
-
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { environment } from 'src/environments/environment';
@@ -165,7 +23,8 @@ export class EventComponent implements OnInit {
     surveys;
     answer;
     idObject;
-    object
+    object;
+    commentWindow = false;
 
     constructor(
         private http: HttpClient, 
@@ -258,4 +117,7 @@ export class EventComponent implements OnInit {
         })
     }
 
+    openComment(){
+        this.commentWindow = !this.commentWindow;
+    }
 }
