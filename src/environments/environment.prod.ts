c274c4c173c8d02b98bc3ff9266bc9075d442ee8
BogdanMoskvitin <moskvitin.bg@gmail.com>
2022-03-04 21:04:32 +0300

    change api.dev to api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.dev.freeteamclub.ru/'
+  apiUrl: 'https://api.freeteamclub.ru/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://api.dev.freeteamclub.ru/'
+  apiUrl: 'https://api.freeteamclub.ru/'
 };
